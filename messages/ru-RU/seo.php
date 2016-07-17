3c458bdb2cfe00f5ac45d74e5eda87e5d01f44b1
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-18 02:11:08 +1000

    Translation added

---
+++ messages/ru-RU/seo.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'SEO' => 'SEO',
+	'Create SEO' => 'Добавить SEO',
+
+	'Changes saved successfully.' => 'Изменения успешно сохранены.',
+	'SEO not found.' => 'SEO информация не найдена.',
+	'SEO deleted successfully.' => 'SEO информация успешно удалена.',
+
+	'Url' => 'Адрес',
+	'Title' => 'Заголовок',
+	'Keywords' => 'Ключевые слова',
+	'Description' => 'Описание',
+	'Snippet' => 'Сниппет',
+
+	'Save' => 'Сохранить',
+	'Cancel' => 'Отмена',
+	'Create' => 'Добавить',
+];
