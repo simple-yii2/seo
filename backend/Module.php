fdca9c282c64d61f9dd967b2e1ca59306ccad270
Dmitry Khlystov <dkhlystov@gmail.com>
2019-04-11 13:28:28 +1000

    Backend module fixed

--- backend/Module.php
+++ backend/Module.php
@@ -9,40 +9,41 @@ use cms\components\BackendModule;
 /**
  * SEO backend module
  */
-class Module extends BackendModule {
+class Module extends BackendModule
+{
+    /**
+     * @inheritdoc
+     */
+    public static function moduleName()
+    {
+        return 'seo';
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function moduleName()
-	{
-		return 'seo';
-	}
+    /**
+     * @inheritdoc
+     */
+    protected static function cmsSecurity()
+    {
+        $auth = Yii::$app->getAuthManager();
+        if ($auth->getRole('SEO') === null) {
+            //seo role
+            $role = $auth->createRole('SEO');
+            $auth->add($role);
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	protected static function cmsSecurity()
-	{
-		$auth = Yii::$app->getAuthManager();
-		if ($auth->getRole('SEO') === null) {
-			//seo role
-			$role = $auth->createRole('SEO');
-			$auth->add($role);
-		}
-	}
+    /**
+     * @inheritdoc
+     */
+    public function cmsMenu()
+    {
+        if (!Yii::$app->user->can('SEO')) {
+            return [];
+        }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function cmsMenu($base)
-	{
-		if (!Yii::$app->user->can('SEO'))
-			return [];
-
-		return [
-			['label' => Yii::t('seo', 'SEO'), 'url' => ["$base/seo/seo/index"]],
-		];
-	}
-
+        return [
+            'label' => Yii::t('seo', 'SEO'),
+            'url' => ['/seo/seo/index'],
+        ];
+    }
 }
